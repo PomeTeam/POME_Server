a4b1c72d45dd77f09986fb129ddef0782ea11bcf
kyubeom <be_man@naver.com>
2022-11-16 15:39:05 +0900

    #1 Project 환경구축

---
+++ src/main/java/com/example/pomeserver/PomeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pomeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomeServerApplication.class, args);
+    }
+
+}

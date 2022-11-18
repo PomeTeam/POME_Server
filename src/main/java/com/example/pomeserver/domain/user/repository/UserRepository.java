1ca8cde76236120e109f59a916777b18796ad9b3
kingchan223 <kokiyo97@naver.com>
2022-11-18 17:57:28 +0900

    user join, login 간단 구현

--- src/main/java/com/example/pomeserver/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/pomeserver/domain/user/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.pomeserver.domain.user.repository;
 
-public interface UserRepository {
+import com.example.pomeserver.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserId(String userId);
 }
--- src/main/java/com/example/pomeserver/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/example/pomeserver/domain/user/service/UserServiceImpl.java
@@ -1,4 +1,35 @@
 package com.example.pomeserver.domain.user.service;
 
+import com.example.pomeserver.domain.global.dto.response.ApplicationResponse;
+import com.example.pomeserver.domain.global.util.SHA256Util;
+import com.example.pomeserver.domain.user.dto.request.UserLoginRequest;
+import com.example.pomeserver.domain.user.dto.request.UserSaveRequest;
+import com.example.pomeserver.domain.user.dto.response.UserResponse;
+import com.example.pomeserver.domain.user.entity.User;
+import com.example.pomeserver.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public ApplicationResponse<UserResponse> create(UserSaveRequest userSaveRequest) {
+        //TODO userId 이미 존재 여부 확인하기
+        userSaveRequest.setEncPassword(SHA256Util.encrypt(userSaveRequest.getPassword()));
+        User user = userRepository.save(userSaveRequest.toEntity());
+        return ApplicationResponse.create("회원가입 완료.", UserResponse.toDto(user));
+    }
+
+    @Override
+    public ApplicationResponse<UserResponse> login(UserLoginRequest userLoginRequest) {
+        User user = userRepository.findByUserId(userLoginRequest.getUserId())
+                .orElseThrow(() -> new RuntimeException("찾을 수 없는 userId"));//TODO Exception 정리
+        if(!user.getPassword().equals(SHA256Util.encrypt(userLoginRequest.getPassword())))
+            throw new RuntimeException("패스워드가 일치하지 않습니다.");
+        return ApplicationResponse.ok(UserResponse.toDto(user));
+    }
 }

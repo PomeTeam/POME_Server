41df4ef095fe188cedbf633ba40602540bd7815b
kingchan223 <kokiyo97@naver.com>
2022-11-23 12:17:55 +0900

    #5 UserServiceImpl.class에 @Transactional 어노테이션 추가

--- src/main/java/com/example/pomeserver/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/example/pomeserver/domain/user/service/UserServiceImpl.java
@@ -12,13 +12,16 @@ import com.example.pomeserver.domain.user.exception.UserIdNotFound;
 import com.example.pomeserver.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
 
+    @Transactional
     @Override
     public ApplicationResponse<UserResponse> create(UserSaveRequest userSaveRequest){
         if(userRepository.findByUserId(userSaveRequest.getUserId()).isPresent())
@@ -31,7 +34,7 @@ public class UserServiceImpl implements UserService{
     @Override
     public ApplicationResponse<UserResponse> login(UserLoginRequest userLoginRequest) {
         User user = userRepository.findByUserId(userLoginRequest.getUserId())
-                .orElseThrow(() -> new UserIdNotFound(userLoginRequest.getUserId()));//TODO Exception 정리
+                .orElseThrow(() -> new UserIdNotFound(userLoginRequest.getUserId()));
         if(!user.getPassword().equals(SHA256Util.encrypt(userLoginRequest.getPassword())))
             throw new PasswordIsNotValid();
         return ApplicationResponse.ok(UserResponse.toDto(user));

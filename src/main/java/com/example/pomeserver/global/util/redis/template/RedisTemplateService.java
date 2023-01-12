229310959bac642800c1ac867a15777fa9ede908
kingchan223 <kokiyo97@naver.com>
2023-01-12 17:23:06 +0900

    #57 redisTemplate 사용한 레디스 CRUD 클래스 생성

---
+++ src/main/java/com/example/pomeserver/global/util/redis/template/RedisTemplateService.java
@@ -0,0 +1,59 @@
+package com.example.pomeserver.global.util.redis.template;
+
+import com.example.pomeserver.global.util.redis.entity.UserRefreshToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.constraints.NotNull;
+
+@RequiredArgsConstructor
+@Service
+public class RedisTemplateService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+
+
+    /**
+     * redis 리프레시 토큰 조회 기능
+     * @Author 이찬영
+     */
+    @Nullable
+    @Transactional(readOnly = true)
+    public String getUserRefreshToken(@NotNull String userId) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(userId);
+    }
+
+    /**
+     * redis 리프레시 토큰 저장 기능
+     * @Author 이찬영
+     */
+    public void saveUserRefreshToken(@NotNull String userId, @NotNull String refreshToken) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(userId, refreshToken);
+    }
+
+    /**
+     * redis 리프레시 토큰 수정 기능
+     * @Author 이찬영
+     */
+    public void updateUserRefreshToken(@NotNull String userId, @NotNull String refreshToken) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.getAndDelete(userId);
+        saveUserRefreshToken(userId, refreshToken);
+    }
+
+    /**
+     * redis 리프레시 토큰 삭제 기능
+     * @Author 이찬영
+     */
+    public void deleteUserRefreshToken(@NotNull String userId){
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.getAndDelete(userId);
+    }
+}

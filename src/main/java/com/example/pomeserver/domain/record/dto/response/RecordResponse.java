ff4b91cbaaf0fe0813780e4dd35b0b16cf1de554
kingchan223 <kokiyo97@naver.com>
2023-01-23 16:25:14 +0900

    #45 RecordResponse에 emotion 값 추가

--- src/main/java/com/example/pomeserver/domain/record/dto/response/RecordResponse.java
+++ src/main/java/com/example/pomeserver/domain/record/dto/response/RecordResponse.java
@@ -1,10 +1,15 @@
 package com.example.pomeserver.domain.record.dto.response;
 
+import com.example.pomeserver.domain.record.entity.EmotionRecord;
 import com.example.pomeserver.domain.record.entity.Record;
+import com.example.pomeserver.domain.record.entity.vo.EmotionType;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ApiModel("기록 응답 객체")
 @NoArgsConstructor
@@ -20,8 +25,12 @@ public class RecordResponse{
     @ApiModelProperty(value = "기록 작성 날짜", example = "2023.02.23", required = true, dataType = "string")
     private String useDate;
 
-    @ApiModelProperty(value = "기록 코멘트", example = "이런저런 소비를 하였다. ^.^", required = true, dataType = "string")
+    @ApiModelProperty(value = "기록 코멘트", example = "이런저런 소비를 하였다(^.^)", required = true, dataType = "string")
     private String useComment;
+
+    @ApiModelProperty(value = "감정 응답(나의 첫번째, 두번째, 친구들 감정 포함)",
+            example = "{firstEmotion:, secondEmotion:, friendEmotions:[]}", required = true, dataType = "json")
+    private EmotionResponse emotionResponse;
 
 
     public static RecordResponse toDto(Record record){
@@ -30,6 +39,9 @@ public class RecordResponse{
         response.usePrice = record.getUsePrice();
         response.useDate = record.getUseDate();
         response.useComment = record.getUseComment();
+        response.emotionResponse = EmotionResponse.toDto(record);
         return response;
     }
+
+
 }

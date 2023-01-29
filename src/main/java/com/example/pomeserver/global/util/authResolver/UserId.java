5b3a9af0110c2872c3f08ae6be8c1ca97f34f933
dldmsql <yhy5913@gmail.com>
2023-01-29 16:43:15 +0900

    #105 스웨거에서 userId 입력칸이 미노출

--- src/main/java/com/example/pomeserver/global/util/authResolver/UserId.java
+++ src/main/java/com/example/pomeserver/global/util/authResolver/UserId.java
@@ -4,8 +4,10 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
+@ApiIgnore
 public @interface UserId {
 }

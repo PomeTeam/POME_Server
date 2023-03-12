3ccfc7b9f1eb9ee312b4207b32d9924d23260aec
kingchan223 <kokiyo97@naver.com>
2023-03-12 15:26:29 +0900

    #208 상수들을 인터페이스 안의 상수 보관 클래스로 옮기기

--- src/test/java/com/example/pomeserver/domain/record/service/RecordTest.java
+++ src/test/java/com/example/pomeserver/domain/record/service/RecordTest.java
@@ -28,6 +28,7 @@ import com.example.pomeserver.domain.record.entity.Record;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import static com.example.pomeserver.domain.record.service.RecordTest.staticValues.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -71,45 +72,62 @@ public class RecordTest {
         Emotion emotion = getTestEmotion1();
         Record record = toEntity(request, goal, user);
         EmotionRecord emotionRecord = getTestMyFirstEmotionRecord(user, emotion, record);
-        RecordResponse recordResponse = getRecordResponse();
 
         //when
         lenient().when(userRepository.findByUserId(any())).thenReturn(Optional.of(user));
         lenient().when(goalRepository.findById(any())).thenReturn(Optional.of(goal));
-        lenient().when(emotionRepository.findById(request.getEmotionId())).thenReturn(Optional.of(emotion));
+        lenient().when(emotionRepository.findById(any())).thenReturn(Optional.of(emotion));
         lenient().when(recordAssembler.toEntity(any(), any(), any())).thenReturn(record);
         lenient().when(emotionRecordAssembler.toEntity(any(), any(), any(), any())).thenReturn(emotionRecord);
         lenient().when(recordRepository.save(any())).thenReturn(record);
+
         ApplicationResponse<RecordResponse> response = recordService.create(request, userId);
 
         //then
         RecordResponse result = response.getData();
         assertAll(
                 () -> assertEquals(request.getUseComment(), result.getUseComment()),
-                () -> assertEquals(request.getUsePrice(), result.getUsePrice())
+                () -> assertEquals(request.getUsePrice(), result.getUsePrice()),
+                () -> assertEquals(request.getUseDate(), result.getUseDate()),
+                () -> assertEquals(request.getEmotionId(), result.getEmotionResponse().getFirstEmotion())
         );
     }
 
-    private RecordResponse getRecordResponse() {
-        return RecordResponse.builder()
-                .id(1L)
-                .nickname("peace")
-                .usePrice(1000)
-                .useDate("2022.03.08")
-                .useComment("테스트용 코멘트")
-                .oneLineMind("testOneLineMind")
-                .createdAt("2022.03.08").emotionResponse(null).build();
 
+
+
+
+
+
+
+
+
+    private RecordResponse getRecordResponse()
+    {
+        return RecordResponse.builder()
+                .id(record1Id)
+                .nickname(user1Nickname)
+                .usePrice(record1UsePrice)
+                .useDate(record1UseDate)
+                .useComment(record1UseComment)
+                .oneLineMind(goal1OneLineMind)
+                .createdAt(record1CreatedAt).emotionResponse(null).build();
     }
 
-    private EmotionRecord getTestMyFirstEmotionRecord(User user, Emotion emotion, Record record){
+    private EmotionRecord getTestMyFirstEmotionRecord(User user,
+                                                      Emotion emotion,
+                                                      Record record)
+    {
         return EmotionRecord.builder().emotionType(EmotionType.MY_FIRST)
                 .user(user)
                 .emotion(emotion)
                 .record(record).build();
     }
 
-    private Record toEntity(RecordCreateRequest request, Goal goal, User user) {
+    private Record toEntity(RecordCreateRequest request,
+                            Goal goal,
+                            User user)
+    {
         Record record = Record.builder()
                 .goal(goal)
                 .user(user)
@@ -120,42 +138,72 @@ public class RecordTest {
         return record;
     }
 
-    private Emotion getTestEmotion1() {
+    private Emotion getTestEmotion1()
+    {
         return Emotion.builder()
-                .id(1L)
-                .emotionName("hi").
-                image("no").build();
+                .id(emotion1Id)
+                .emotionName(emotion1Name).
+                image(emotion1Image).build();
     }
 
 
-    private Goal getTestGoal1() {
+    private Goal getTestGoal1()
+    {
         return Goal.builder()
-                .name("testGoalName")
-                .startDate("2023.03.01")
-                .endDate("2023.03.25")
-                .oneLineMind("testOneLineMind")
-                .price(50000)
-                .isPublic(true)
+                .name(goal1Name)
+                .startDate(goal1StartDate)
+                .endDate(goal1EndDate)
+                .oneLineMind(goal1OneLineMind)
+                .price(goal1Price)
+                .isPublic(goal1IsPublic)
                 .user(getTestUser1()).build();
     }
 
-    private RecordCreateRequest getRecordCreateRequest() {
+    private RecordCreateRequest getRecordCreateRequest()
+    {
         return RecordCreateRequest.builder()
-                .goalId(1L)
-                .emotionId(1L)
-                .usePrice(1000)
-                .useDate("2023.03.08")
-                .useComment("테스트용 코멘트").build();
+                .goalId(goal1Id)
+                .emotionId(emotion1Id)
+                .usePrice(record1UsePrice)
+                .useDate(record1UseDate)
+                .useComment(record1UseComment).build();
     }
 
-    private User getTestUser1() {
+    private User getTestUser1()
+    {
         return User.builder()
-                .userId("userId1")
-                .nickname("peace")
-                .phoneNum("01011112222")
-                .image("myImage").build();
+                .userId(user1Id)
+                .nickname(user1Nickname)
+                .phoneNum(user1PhoneNumber)
+                .image(user1Image).build();
     }
 
+    public interface staticValues{
+        /* user */
+        String user1Id = "userId1";
+        String user1Nickname = "peace";
+        String user1PhoneNumber = "01011112222";
+        String user1Image = "user1Image";
 
+        /* goal */
+        Long goal1Id = 1L;
+        String goal1Name = "goal1Name";
+        String goal1StartDate = "2023.03.01";
+        String goal1EndDate = "2023.03.25";
+        String goal1OneLineMind = "goal1OneLineMind";
+        int goal1Price = 50000;
+        Boolean goal1IsPublic = true;
 
+        /* emotion */
+        Long emotion1Id = 1L;
+        String emotion1Name = "smile";
+        String emotion1Image = "emoji1";
+
+        /* record */
+        Long record1Id = 1L;
+        int record1UsePrice = 1000;
+        String record1UseDate = "2022.03.08";
+        String record1UseComment = "테스트1용 코멘트";
+        String record1CreatedAt = "2022.03.08";
+    }
 }
